a7b744b53f2e0ded9381fd228174c11d96294e50
Toridia <alinapotiavina@mail.ru>
2025-12-09 17:04:18 +0300

    Create AuthInput.jsx

---
+++ my-project/src/Pages/Auth/UI/AuthInput.jsx
@@ -0,0 +1,11 @@
+
+function AuthInput() {
+    return (
+        <div className="row-style">
+            <input className="App-input"></input>
+            <input className="App-input"></input>
+        </div>
+            )
+}
+
+export default AuthInput;
