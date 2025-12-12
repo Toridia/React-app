5bba74de6067e2dd2a49bf2a0fece58521af1b3e
Toridia <alinapotiavina@mail.ru>
2025-12-12 11:00:10 +0300

    Create GroupCard.jsx

---
+++ src/pages/Group/UI/GroupCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './GroupCard.css';
+
+const GroupCard = ({ group, onClick }) => {
+  return (
+    <div className="group-card" onClick={onClick}>
+      <h3>{group.name}</h3>
+      <div className="group-members">{group.memberCount} участников</div>
+      <div className="group-actions">
+        <button className="action-btn" onClick={(e) => {
+          e.stopPropagation();
+          onClick();
+        }}>
+          Открыть
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default GroupCard;
